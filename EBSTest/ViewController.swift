8a310f78705241822985f7f0f80de7ff2a54b079
gardred <priseajniuc2000@mal.ru>
2022-05-03 09:07:40 +0300

    Initial Commit

---
+++ EBSTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EBSTest
+//
+//  Created by Сережа Присяжнюк on 03.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
